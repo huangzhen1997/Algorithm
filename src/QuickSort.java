c93898e1374ba13257a4edafcd79840d6e3004d3
huangzhen1997 <zhuang38@hawk.iit.edu>
2019-08-22 16:29:06 +0800

    adding comments

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,24 +1,11 @@
-public class Leetcode1 {
+public class QuickSort {
 
     public static void main(String[] args){
-
-
+        
         int[] array = {2,4,1,6,3,7,9,0};
+        
         quicksort(array,0,array.length-1);
-        String output = "[";
-
-
-        for(int i=0;i<array.length;i++){
-
-            output = output+(Integer.valueOf(array[i]));
-
-            if(i!=array.length-1){
-                output = output+",";
-            }
-            else{
-                output=output+"]";
-            }
-        }
+        String output = outputformat(array);
         System.out.println(output);
     }
 
@@ -32,32 +19,32 @@ public class Leetcode1 {
 
     public static int partition(int[] array, int low, int high){
 
-        int leftwall = low;
+        int leftWall = low;
         int pivot= array[low];
         int pivot_index= low;//could be optimize by selecting three-medium
-
 
         for(int i = low+1; i <=high ; i++){
 
             if(array[i]<pivot){
 
-                //These two if condition is simply keeping track of where the pivot goes.
-                if (leftwall==pivot_index){
+                //These two if-conditions are just simply keeping track of where the pivot goes.
+                if (leftWall==pivot_index){
                     pivot_index=i;
                 }
                 else if (i == pivot_index){
-                    pivot_index=leftwall;
+                    pivot_index=leftWall;
                 }
-
-                swap(array,i,leftwall);
-                leftwall=leftwall+1;
+                
+                //swap the leftWall so that everything on the LHS of it are smaller and RHS are bigger
+                swap(array,i,leftWall);
+                leftWall=leftWall+1;
 
             }
 
         }
 
-        swap(array,leftwall,pivot_index);
-        return leftwall;
+        swap(array,leftWall,pivot_index);
+        return leftWall;
     }
 
     public static void swap(int[] array,int pos1,int pos2){
@@ -66,5 +53,21 @@ public class Leetcode1 {
         array[pos1]=array[pos2];
         array[pos2]=temp;
 
+    }
+
+
+
+    public static String outputformat(int[] array){
+        String output = "[";
+        for(int i=0;i<array.length;i++){
+            output = output+(Integer.valueOf(array[i]));
+            if(i!=array.length-1){
+                output = output+",";
+            }
+            else{
+                output=output+"]";
+            }
+        }
+        return output;
     }
 }
