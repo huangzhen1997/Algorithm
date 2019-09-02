3d6a63d489d4ae931f59175cb56147773cae00f3
Zhen Huang <zhuang38@hawk.iit.edu>
2019-09-02 12:58:15 +0800

    LongestSubstring

---
+++ src/LongestSubstring_Brute_Force.java
@@ -0,0 +1,59 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstring {
+
+    public static void main(String[] args){
+
+        String input = "abcabcbb";
+        int output = lengthOfLongestSubstring(input);
+        System.out.println(output);
+
+
+    }
+
+
+    public static boolean noduplicate(String s, int start, int end){
+
+
+        Set<Character> check = new HashSet<>();
+
+        for(int i = start; i < end; i ++){
+
+            if (check.contains(s.charAt(i))){
+
+                return false;
+
+            }
+
+            else {
+
+                check.add(s.charAt(i));
+            }
+        }
+
+        return true;
+
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        int maxlength=0;
+
+        for(int i  = 0; i < s.length();i++){
+
+            for (int j = i+1 ; j <= s.length() ; j++){
+
+                if (noduplicate(s,i,j)){
+
+                    maxlength = Math.max(maxlength,j-i);
+
+                }
+
+            }
+
+        }
+
+        return maxlength;
+    }
+}
---
+++ src/LongestSubstring_Sliding_window.java
@@ -0,0 +1,51 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstring {
+
+
+    public static int lengthOfLongestSubstring(String s){
+
+        int i= 0, j = 0, maxLength = 0;
+        int length = s.length();
+        Set<Character> lookup = new HashSet<>();
+
+        while(i<length && j<length){
+
+            if( ! lookup.contains(s.charAt(j))){
+
+                lookup.add(s.charAt(j));
+                maxLength= Math.max(j-i,maxLength);
+                j++;
+
+
+            }
+
+            else{
+
+                lookup.remove(s.charAt(i++));
+            }
+
+
+
+
+        }
+
+        return maxLength;
+
+    }
+
+
+
+
+
+
+    public static void main(String[] args){
+
+        String input = "abccdba";
+        int output = lengthOfLongestSubstring(input);
+        System.out.println(output);
+
+    }
+
+}
